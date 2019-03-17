b820bf2fc5d31e3da3283f4657ad15bb3bbad224
Abhay Kaushik <abhay.gyanbharati@gmail.com>
2019-03-17 12:44:00 +0530

    Added jump script to character

---
+++ GameDev -1/Assets/Scripts/heroRun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class heroRun : MonoBehaviour
+{
+    public float upForce = 200f;
+
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isDead == false)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                //rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, 2*upForce));
+                anim.SetTrigger("player_jump");
+            }
+
+        }
+    }
+}
